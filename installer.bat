69e0252038ab4e89dc76a9881f4e711434743619
YangHui <tlz.yh@outlool.com>
2017-11-02 22:52:41 +0800

    update

--- installer.bat
+++ installer.bat
@@ -1,5 +1,5 @@
 REM hv installer on Windows
-REM Last Change: 2017/10/30
+REM Last Change: 2017/11/02
 REM Author: YangHui <tlz.yh@outlook.com>
 REM Maintainer: YangHui <tlz.yh@outlook.com>
 REM License: This file is placed in the public domain.
@@ -8,9 +8,9 @@ REM License: This file is placed in the public domain.
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
 
 @set APP_PATH=%HOME%\.hv
-IF NOT EXIST "%APP_PATH%" (
+if not exist "%APP_PATH%" (
     call git clone -b master https://github.com/tlzyh/hv.git "%APP_PATH%"
-) ELSE (
+) else (
     @set ORIGINAL_DIR=%CD%
     echo updating hv...
     chdir /d "%APP_PATH%"
@@ -19,26 +19,29 @@ IF NOT EXIST "%APP_PATH%" (
     call cd "%APP_PATH%"
 )
 
+REM create vim file link
 call mklink "%HOME%\.vimrc" "%APP_PATH%\.vimrc"
 call mklink "%HOME%\_vimrc" "%APP_PATH%\.vimrc"
 call mklink /J "%HOME%\.vim" "%APP_PATH%\.vim"
 
-IF NOT EXIST "%APP_PATH%\.vim\plugin" (
+REM create plugin directory
+if not exist "%APP_PATH%\.vim\plugin" (
     call mkdir "%APP_PATH%\.vim\plugin"
 )
 
-IF NOT EXIST "%HOME%/.vim/plugin/vim-plug" (
+if not exist "%HOME%/.vim/plugin/vim-plug" (
     call git clone https://github.com/junegunn/vim-plug.git "%HOME%/.vim/plugin/vim-plug"
-) ELSE (
+) else (
   call cd "%HOME%/.vim/plugin/vim-plug"
   call git pull
   call cd %HOME%
 )
 
 REM copy plug.vim to autoload directory
-IF NOT EXIST "%HOME%\.vim\vimfiles\autoload" (
+if not exist "%HOME%\.vim\vimfiles\autoload" (
     call mkdir "%HOME%\.vim\vimfiles\autoload"
 )
+REM copy plug.vim to autoload directory
 xcopy /Y /Q "%HOME%\.vim\plugin\vim-plug\plug.vim" "%HOME%\.vim\vimfiles\autoload"
 
 call vim -u "%APP_PATH%/.vimrc" +PlugInstall
